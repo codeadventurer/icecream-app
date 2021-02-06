5818953bb32b68bfca9ec6776876f9d7a23f9989
Anna Dontcova <a.dontcova@gmail.com>
2021-02-06 23:22:19 +0100

    adds Options component and tests

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Row } from 'react-bootstrap';
+
+import ScoopOption from './ScoopOption';
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    // optionType is 'scoops' or 'toppings'
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.OptionsimagePath}
+    />
+  ));
+  return <Row>{optionItems}</Row>;
+}

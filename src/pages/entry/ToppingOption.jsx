673ec1b51df9a486b9ea5adb8db11b5418ae3250
Anna Dontcova <a.dontcova@gmail.com>
2021-03-07 17:43:13 +0100

    tests toppings options

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,10 @@
-import { Col } from 'react-bootstrap';
+import { Col, Form, Row } from 'react-bootstrap'
 
-export default function ToppingOption({ name, imagePath }) {
+export default function ToppingOption({ name, imagePath, updateItemCount }) {
+  const toggleChecked = (event) => {
+    updateItemCount(name, +event.target.checked)
+  }
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
@@ -8,6 +12,12 @@ export default function ToppingOption({ name, imagePath }) {
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group
+        controlId={`${name}-tooping-checkbox`}
+        style={{ marginTop: '10px' }}
+      >
+        <Form.Check type="checkbox" label={name} onChange={toggleChecked} />
+      </Form.Group>
     </Col>
-  );
+  )
 }
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,10 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '../../../test-utils/testing-library-utils'
 import userEvent from '@testing-library/user-event'
 import Options from '../Options'
-import { OrderDetailsProvider } from '../../../contexts/OrderDetails'
 
 test('update scoop subtotal when scoops change', async () => {
-  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider })
+  render(<Options optionType="scoops" />)
 
   // make sure subtotal starts at 0.00$
 
@@ -28,4 +27,37 @@ test('update scoop subtotal when scoops change', async () => {
   userEvent.clear(chocolateInput)
   userEvent.type(chocolateInput, '2')
   expect(scoopsSubtotal).toHaveTextContent('6.00')
+})
+
+test('update topping subtotal when toppings change', async () => {
+  render(<Options optionType="toppings" />)
+
+  // make sure subtotal starts at 0.00$
+  const toppingsSubtotal = screen.getByText('Toppings total: $', {
+    exact: false,
+  })
+  expect(toppingsSubtotal).toHaveTextContent('0.00')
+
+  // update Cherries topping to checked and see subtotal
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
+    name: 'Cherries',
+  })
+  expect(cherriesCheckbox).not.toBeChecked()
+  userEvent.click(cherriesCheckbox)
+  expect(cherriesCheckbox).toBeChecked()
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
+
+  // update M&Ms topping to checked and see subtotal
+  const MAndMsCheckbox = await screen.findByRole('checkbox', {
+    name: 'M&Ms',
+  })
+  expect(MAndMsCheckbox).not.toBeChecked()
+  userEvent.click(MAndMsCheckbox)
+  expect(MAndMsCheckbox).toBeChecked()
+  expect(toppingsSubtotal).toHaveTextContent('3.00')
+
+  // uncheck Cherries topping and see subtotal
+  userEvent.click(MAndMsCheckbox)
+  expect(MAndMsCheckbox).not.toBeChecked()
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
 })
